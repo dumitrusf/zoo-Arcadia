eaf78e8d32f9a1908584ed6fd4db7d32b2d3b4fc
Dumitru Stefan-Fernando <dumitrulovelace@gmail.com>
2025-07-25 18:02:24 +0200

    chore: a missunderstanding issue at my fault with mvc and the inclusion of partials

---
+++ App/employees/views/pages/intro.php
@@ -0,0 +1,17 @@
+<body>
+<div class="p-5 bg-dark">
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Bienvenido al Panel de Empleados</h1>
+            <p class="col-md-8 fs-4">
+                Desde este panel podrás gestionar la información de los empleados del zoológico Arcadia. Utiliza el menú de navegación para acceder a las diferentes funcionalidades disponibles.
+            </p>
+            <p class="col-md-8 fs-4">
+                3674895923465283476987✨✨✨
+            </p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Ver empleados</a>
+        </div>
+    </div>
+</div>
+</body>
+
