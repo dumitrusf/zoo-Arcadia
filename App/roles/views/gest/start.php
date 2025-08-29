e27b26912230a086cd6ef5a89a4b5010a4850c59
Dumitru Stefan-Fernando <dumitrulovelace@gmail.com>
2025-08-29 17:02:33 +0200

    WIP from laptop (click to see what happened)

--- App/roles/views/gest/start.php
+++ App/roles/views/gest/start.php
@@ -1,41 +1,36 @@
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
-        <h2 class="card-title">Employees</h2>
-        <a name="employees" id="" class="btn btn-success mb-2 mt-2" href="?controller=gest&action=create" role="button">Add Employee</a>
+        <h2 class="card-title">roles</h2>
+        <a name="roles" id="" class="btn btn-success mb-2 mt-2" href="?controller=gest&action=create" role="button">Add Employee</a>
     </div>
     <div class="card-body">
-        <!-- Formulario para ver empleados -->
+        <!-- Formulario para ver roles (puestos) -->
         <table class="table table-bordered table-striped table-hover">
             <thead>
                 <tr>
                     <!-- <th scope="col">ID</th> -->
-                    <th scope="col">Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Role</th>
+                    <th scope="col">Role-Name</th>
+                    <th scope="col">Role-Description</th>
                     <th scope="col">Creado el</th>
                     <th scope="col">Editado el</th>
-                    <th scope="col">Actions</th>
                 </tr>
             </thead>
             <tbody>
 
-                <?php foreach ($employees as $employee) { ?>
+                <?php foreach ($roles as $role) { ?>
 
                     <tr>
-                        <!-- <td> <?php echo $employee->id; ?> </td> -->
-                        <td> <?php echo $employee->first_name; ?> </td>
-                        <td> <?php echo $employee->last_name; ?> </td>
-                        <td> <?php echo $employee->email; ?> </td>
-                        <td> <?php echo $employee->role_id; ?> </td>
-                        <td> <?php echo $employee->created_at; ?> </td>
-                        <td> <?php echo $employee->updated_at; ?> </td>
+                        <!-- <td> <?php echo $role->id; ?> </td> -->
+                        <td> <?php echo $role->role_name; ?> </td>
+                        <td> <?php echo $role->role_description; ?> </td>
+                        <td> <?php echo $role->created_at; ?> </td>
+                        <td> <?php echo $role->updated_at; ?> </td>
                         <td>
 
                             <div class="btn-group" role="group" aria-label="">
 
-                                <a href="?controller=gest&action=edit&id=<?php echo $employee->id; ?>" class="btn btn-sm btn-primary">Edit</a>
-                                <a href="?controller=gest&action=delete&id=<?php echo $employee->id; ?>" class="btn btn-sm btn-danger">Delete</a>
+                                <a href="?domain=roles&controller=gest&action=edit&id=<?php echo $role->id; ?>" class="btn btn-sm btn-primary">Edit</a>
+                                <a href="?domain=roles&controller=gest&action=delete&id=<?php echo $role->id; ?>" class="btn btn-sm btn-danger">Delete</a>
                             </div>
 
                         </td>
